5e671b93f9fd736b43d0f7ab6035798e36a84371
Jinwoong Shin <jinwoong7116@gmail.com>
2022-04-28 14:55:59 +0900

    1
    1

---
+++ VR Room Project/Assets/_Course Library/Script/ItemClick.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ItemClick : MonoBehaviour
+{
+    public UnityEvent unityEvent;
+    public GameObject button;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
+            {
+                unityEvent.Invoke();
+            }
+        }
+    }
+}
